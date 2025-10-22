375c73aaee51ace436d40864059286d468b61fd0
Nick Mudge <nick@perfectabstractions.com>
2025-10-21 21:55:13 -0400

    Created LibUtils with toString

--- src/ERC721/ERC721/ERC721Facet.sol
+++ src/ERC721/ERC721/ERC721Facet.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.30;
 
+import { LibUtils } from "../../Libraries/LibUtils.sol";
+
 /// @title ERC-721 Token Receiver Interface
 /// @notice Interface for contracts that want to handle safe transfers of ERC-721 tokens.
 /// @dev Contracts implementing this must return the selector to confirm token receipt.
@@ -255,66 +257,8 @@ contract ERC721Facet {
             return "";
         }
 
-        return string.concat(s.baseURI, toString(_tokenId));
+        return string.concat(s.baseURI, LibUtils.toString(_tokenId));
     }
 
-    /// From openzeppelin/contracts/utils/Strings.sol
-    /// @dev Converts a `uint256` to its ASCII `string` decimal representation.
-    function toString(uint256 value) internal pure returns (string memory) {
-        bytes16 _SYMBOLS = "0123456789abcdef";
-        unchecked {
-            uint256 length = log10(value) + 1;
-            string memory buffer = new string(length);
-            uint256 ptr;
-            assembly ("memory-safe") {
-                ptr := add(buffer, add(32, length))
-            }
-            while (true) {
-                ptr--;
-                assembly ("memory-safe") {
-                    mstore8(ptr, byte(mod(value, 10), _SYMBOLS))
-                }
-                value /= 10;
-                if (value == 0) break;
-            }
-            return buffer;
-        }
-    }
 
-    /// From openzeppelin/contracts/utils/Math.sol
-    /// @dev Return the log in base 10 of a positive value rounded towards zero.
-    /// Returns 0 if given 0.
-    function log10(uint256 value) internal pure returns (uint256) {
-        uint256 result = 0;
-        unchecked {
-            if (value >= 10 ** 64) {
-                value /= 10 ** 64;
-                result += 64;
-            }
-            if (value >= 10 ** 32) {
-                value /= 10 ** 32;
-                result += 32;
-            }
-            if (value >= 10 ** 16) {
-                value /= 10 ** 16;
-                result += 16;
-            }
-            if (value >= 10 ** 8) {
-                value /= 10 ** 8;
-                result += 8;
-            }
-            if (value >= 10 ** 4) {
-                value /= 10 ** 4;
-                result += 4;
-            }
-            if (value >= 10 ** 2) {
-                value /= 10 ** 2;
-                result += 2;
-            }
-            if (value >= 10 ** 1) {
-                result += 1;
-            }
-        }
-        return result;
-    }
 }
