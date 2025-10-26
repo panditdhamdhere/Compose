0dac8ebbce732c79d316f05ea754c3a0d37ec09e
Maxime Normandin <maxnorm@hotmail.com>
2025-10-26 11:22:39 -0400

    fix formatting issue on CI

--- src/diamond/DiamondCutFacet.sol
+++ src/diamond/DiamondCutFacet.sol
@@ -107,7 +107,8 @@ contract DiamondCutFacet {
             if (oldFacetAddressAndSelectorPosition.selectorPosition != selectorCount) {
                 bytes4 lastSelector = ds.selectors[selectorCount];
                 ds.selectors[oldFacetAddressAndSelectorPosition.selectorPosition] = lastSelector;
-                ds.facetAddressAndSelectorPosition[lastSelector].selectorPosition = oldFacetAddressAndSelectorPosition.selectorPosition;
+                ds.facetAddressAndSelectorPosition[lastSelector].selectorPosition =
+                oldFacetAddressAndSelectorPosition.selectorPosition;
             }
             // delete last selector
             ds.selectors.pop();
