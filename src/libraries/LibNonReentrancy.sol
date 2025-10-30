a94b460d84d6f3ee5cc17d9b43c5c29396df4f4a
Nick Mudge <nick@perfectabstractions.com>
2025-10-30 08:07:03 -0400

    reentrancy

--- src/libraries/LibNonReentrancy.sol
+++ src/libraries/LibNonReentrancy.sol
@@ -4,13 +4,23 @@ pragma solidity >=0.8.30;
 /// @title LibNonReentrancy - Non-Reentrancy Library
 /// @notice Provides common non-reentrant functions for Solidity contracts.
 library LibNonReentrancy {
-    bytes32 constant NON_REENTRANT_SLOT = keccak256("compose.nonReentrant");
+    bytes32 constant NON_REENTRANT_SLOT = keccak256("compose.nonreentrant");
 
     // Function selector - 0x43a0d067
     error Reentrancy();
 
+     /// @dev How to use as a library in user facets
+    /*
+    function someFunction() external {
+        LibNonReentrancy.enter();
+        // functions logic here...
+        LibNonReentrancy.exit();
+    }
+    */
+
     /// @dev How to use as a modifier in user facets
-    /*modifier nonReentrant {
+    /*
+    modifier nonReentrant {
         LibNonReentrancy.enter();
         _;
         LibNonReentrancy.exit();
