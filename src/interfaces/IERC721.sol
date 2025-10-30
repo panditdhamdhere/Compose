a7562f7a80ee99fb68bfa244aa0feb3114394cc1
aapsi <aapsikhaira98@gmail.com>
2025-10-30 19:23:27 +0530

    refactor: move IERC721Receiver interface to its own file

--- src/interfaces/IERC721.sol
+++ src/interfaces/IERC721.sol
@@ -101,18 +101,3 @@ interface IERC721 {
     /// @return the URI providing the detailed metadata of the specified tokenID
     function tokenURI(uint256 _tokenId) external view returns (string memory);
 }
-
-/// @title ERC-721 Token Receiver Interface
-/// @notice Interface for contracts that want to handle safe transfers of ERC-721 tokens.
-/// @dev Contracts implementing this must return the selector to confirm token receipt.
-interface IERC721Receiver {
-    /// @notice Handles the receipt of an NFT.
-    /// @param _operator The address which called `safeTransferFrom`.
-    /// @param _from The previous owner of the token.
-    /// @param _tokenId The NFT identifier being transferred.
-    /// @param _data Additional data with no specified format.
-    /// @return The selector to confirm the token transfer.
-    function onERC721Received(address _operator, address _from, uint256 _tokenId, bytes calldata _data)
-        external
-        returns (bytes4);
-}
